489ab3ddf9b51521af2e2acd1c0a6131efe44971
Ajeetesh <95238154+Ajeetesh2003@users.noreply.github.com>
2023-10-25 18:06:13 +0530

    Update 7_SequenceInRollingOfDice.py

--- 7_SequenceInRollingOfDice.py
+++ 7_SequenceInRollingOfDice.py
@@ -10,3 +10,4 @@ Pick one of the choices
     0.56
     None of the above
 """
+Ans: https://www.quora.com/If-you-roll-a-10-sided-dice-3-times-what-is-the-probability-that-they-form-neither-a-strictly-increasing-nor-decreasing-series#:~:text=So%20the%20probability%20of%20a,decreasing%20series%20is%20760%2F1000.
